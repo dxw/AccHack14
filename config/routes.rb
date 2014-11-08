b410c7626164bde0e41b83ec41b8a6ba7446b605
Duncan Stuart <dgmstuart@gmail.com>
2014-11-08 21:21:11 +0000

    forgot from last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 	root to: "welcome#index"
 
+  resources :local_authorities, only: [:index, :show]
+
 	get '/about', to: 'welcome#about'
   get '/local_authorities', to: 'data_views#local_authorities'
   get '/homelessnesses', to: 'data_views#homelessnesses'
