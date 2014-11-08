df1e1568dcb8a4f71bd315cd2ecb2fb3e2329f03
Magdalena Faizov <m.maciaszczyk@gmail.com>
2014-11-08 18:03:40 +0000

    about page, active nav, more fonts and colours

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	root to: "welcome#index"
 
-	resources :welcome
+	get '/about', to: 'welcome#about'
   get '/local_authorities', to: 'data_views#local_authorities'
 end
