bd53957e371be403150dc1da90a106b30a2f9411
Duncan Stuart <dgmstuart@gmail.com>
2014-11-08 18:37:07 +0000

    sorting values by numbers

--- app/controllers/data_views_controller.rb
+++ app/controllers/data_views_controller.rb
@@ -3,9 +3,9 @@ class DataViewsController < ApplicationController
     @local_authorities = LocalAuthority.all
   end
   def homelessnesses
-    @homelessnesses = Homelessness.all
+    @homelessnesses = Homelessness.all.order(type_1: :desc)
   end
   def social_housing_sales
-    @social_housing_sales = SocialHousingSale.all
+    @social_housing_sales = SocialHousingSale.all.order(sales: :desc)
   end
 end
