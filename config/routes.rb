f3fcf5f9f648c4146dfc0e6b3f5325453306251b
Duncan Stuart <dgmstuart@gmail.com>
2014-11-09 09:18:29 +0000

    making Local Authorities the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-	root to: "welcome#index"
+	root to: "local_authorities#index"
 
   resources :local_authorities, only: [:index, :show]
 
 	get '/about', to: 'welcome#about'
-  get '/local_authorities', to: 'data_views#local_authorities'
   get '/homelessnesses', to: 'data_views#homelessnesses'
   get '/social_housing_sales', to: 'data_views#social_housing_sales'
   get '/social_housing', to: 'data_views#social_housing'
