a35c9a24c3031026e81750c6119939f646c21467
Duncan Stuart <dgmstuart@gmail.com>
2014-11-09 11:52:49 +0000

    all LA show numbers are now correctly calculated

--- app/controllers/local_authorities_controller.rb
+++ app/controllers/local_authorities_controller.rb
@@ -5,10 +5,10 @@ class LocalAuthoritiesController < ApplicationController
   def show
     @local_authority = LocalAuthority.find_by_la_code(params[:id])
     @households = @local_authority.total_households
-    @social_housing = @local_authority.social_housing.household_total_rent
-    @social_housing_average_households = 0.26
-    @local_authority_percentage_households = @social_housing*100.0/@households
+    @social_housing = @local_authority.number_social_housing
+    @social_housing_average_households = LocalAuthority.average_social_housing_household_percentage
+    @local_authority_percentage_households = @local_authority.percentage_social_housing
     @homeless_average = 0.1
-    @local_authority_percentage_homeless = @local_authority.number_of_homeless*100.0/@households
+    @local_authority_percentage_homeless = @local_authority.percentage_homeless
   end
 end
--- app/models/local_authority.rb
+++ app/models/local_authority.rb
@@ -5,4 +5,29 @@ class LocalAuthority < ActiveRecord::Base
   def number_of_homeless
     homelessness.type_1 + homelessness.type_2 + homelessness.type_3
   end
+
+  def number_social_housing
+    social_housing.household_total_rent
+  end
+  def percentage_social_housing
+    number_social_housing*100.0/total_households
+  end
+
+
+  def percentage_homeless
+    number_of_homeless*100.0/total_households
+  end
+
+  def self.average_social_housing_household_percentage
+    return 0 if count == 0
+    sum = all.to_a.sum(&:percentage_social_housing)
+    sum.to_f/count
+  end
+  def self.average_homeless_percentage
+    return 0 if count == 0
+    sum = all.to_a.sum(&:percentage_homeless)
+    sum.to_f/count
+  end
+
+
 end
