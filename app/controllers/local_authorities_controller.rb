7c2f8ede4a4f2ecb19ff815c983f53f819642d81
Duncan Stuart <dgmstuart@gmail.com>
2014-11-09 12:40:59 +0000

    Correcting the data for percentage houses sold

--- app/controllers/local_authorities_controller.rb
+++ app/controllers/local_authorities_controller.rb
@@ -8,10 +8,12 @@ class LocalAuthoritiesController < ApplicationController
     @social_housing = @local_authority.number_social_housing
     @social_housing_average_households = LocalAuthority.average_social_housing_household_percentage
     @local_authority_percentage_households = @local_authority.percentage_social_housing
+
     @homeless_average = LocalAuthority.average_homeless_percentage
     @local_authority_percentage_homeless = @local_authority.percentage_homeless
 
     @local_authority_houses_sold = @local_authority.houses_sold
+    @local_authority_percentage_houses_sold = @local_authority.percentage_houses_sold
     @houses_sold_average = LocalAuthority.average_houses_sold
   end
 end
