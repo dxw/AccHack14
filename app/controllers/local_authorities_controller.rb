32dacfdceceb13a9a463f6db7e5f0cce1fb747b7
Lily Dart <lily@dxw.com>
2014-11-09 09:38:01 +0000

    Changing fake data into really badly named variables

--- app/controllers/local_authorities_controller.rb
+++ app/controllers/local_authorities_controller.rb
@@ -4,5 +4,11 @@ class LocalAuthoritiesController < ApplicationController
   end
   def show
     @local_authority = LocalAuthority.find_by_la_code(params[:id])
+    @households = 1257
+    @social_housing = 300
+    @social_housing_average_households = 0.26
+    @local_authority_percentage_households = 0.32
+    @homeless_average = 0.1
+    @local_authority_percentage_homeless = 0.05
   end
 end
