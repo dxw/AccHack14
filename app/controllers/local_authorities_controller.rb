3cf137c38254184b212a8eb3a6c98bf30723a092
Hippers <tomhipkin@gmail.com>
2014-11-09 11:54:37 +0000

    sort local auths by name

--- app/controllers/local_authorities_controller.rb
+++ app/controllers/local_authorities_controller.rb
@@ -1,6 +1,6 @@
 class LocalAuthoritiesController < ApplicationController
   def index
-    @local_authorities = LocalAuthority.all
+    @local_authorities = LocalAuthority.all.sort{|a,b| a.name <=> b.name}
   end
   def show
     @local_authority = LocalAuthority.find_by_la_code(params[:id])
@@ -9,6 +9,9 @@ class LocalAuthoritiesController < ApplicationController
     @social_housing_average_households = 0.26
     @local_authority_percentage_households = @social_housing*100.0/@households
     @homeless_average = 0.1
+    @local_authority_percentage_homeless = 0.05
+    @local_authority_houses_sold = 0.05
+    @houses_sold_average = 0.02
     @local_authority_percentage_homeless = @local_authority.number_of_homeless*100.0/@households
   end
 end
